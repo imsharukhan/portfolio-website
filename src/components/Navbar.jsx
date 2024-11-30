84d28b26858daf3e720027ab43087238090a5d6d
imsharukhan <sharukhansharukhan926@gmail.com>
2024-11-30 21:45:57 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/srkLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+const Navbar = () => {
+  return <nav className="mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+    </div>
+    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+    <a href="https://www.linkedin.com/in/sharuk-s18?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin/>
+      </a>
+        <a href="https://github.com/imsharukhan" target="_blank" rel="noopener noreferrer">
+        <FaGithub/>
+        </a>
+        <FaInstagram />
+        <FaSquareXTwitter />
+    </div>
+  </nav>
+  
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import Awwards from "../assets/projects/Awwards.png";
+import alabey from "../assets/projects/alabey.png";
+import portfolio from "../assets/projects/portfolio.png";
+
+export const HERO_CONTENT = `I am a passionate Web developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Tailwind CSS. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile web developer with a passion for creating efficient and user-friendly web applications. Currently pursuing a Bachelor of Computer Science Engineering in my second year at Dhanalakshmi Srinivasan University, I am developing a strong foundation in technical skills and practical development techniques. My journey in web development began with a deep curiosity for how things work, and it has evolved into a skillset where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Web Developer Intern",
+    company: "CodeSpaze",
+    description: `I developed a comprehensive personal portfolio website to showcase my web development skills and professional journey. The project demonstrates my ability to create a responsive, modern, and visually appealing web application using cutting-edge technologies.`,
+    technologies: ["HTML", "Tailwind CSS","Javascript", "React.js"],
+  },
+  {
+    year: "2024",
+    role: "Web Developer Intern",
+    company: "CodSoft",
+    description: `Created and styled responsive web pages using HTML, CSS, and JavaScript, ensuring cross-browser compatibility and adherence to web standards. Designed layouts that adapt seamlessly across devices, significantly improving the user experience on both mobile and desktop. Collaborated effectively in team projects using Git to manage code versions and maintain code integrity.`,
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    year: "2024",
+    role: "Young Innovator Intern",
+    company: "Scalar",
+    description: `I developed a diverse skill set by creating AI solutions for emerging businesses and building a strong digital presence through effective marketing. I gained experience in no-code website development and designed functional websites while applying sales mastery techniques to enhance customer engagement. Additionally, I created interactive Discord bots to foster community interaction.`,
+    technologies: ["AI", "NoCode Website", "Sales Techniques", "Collab Skill"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Awwards Website",
+    image: Awwards,
+    description:
+      "I developed a fully animated, interactive website using cutting-edge frontend technologies to create an engaging and dynamic user experience. The project showcases my expertise in modern web development techniques and advanced animation capabilities.",
+    technologies: ["HTML", "CSS","Tailwind CSS", "React", "GSAP"],
+    link: "https://sharukwebsite.web.app",
+  },
+  {
+    title: "Alabey Website",
+    image: alabey,
+    description:
+      " I developed the frontend for the Alabey website, focusing on creating an engaging and user-friendly interface using HTML, CSS, and JavaScript. I implemented responsive design principles to ensure optimal viewing across various devices, enhancing the overall user experience.",
+    technologies: ["HTML", "CSS", "Javascript"],
+    link: "https://alabey-world.netlify.app/",
+  },
+  {
+    title: "Portfolio Website",
+    image: portfolio,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["Javascript", "React", "Tailwind CSS"],
+  },
+];
+
+export const CONTACT = {
+  address: "Tvs Tollgate, Trichy, Tamilnadu ",
+  phoneNo: "+91-6379764854 ",
+  email: "sharukhansharukhan926@gmail.com",
+};
